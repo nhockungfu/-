b80f8b249c20b83431a2af688169a8563fca1ed0
lvphuc253 <lvphucitus@gmail.com>
2017-06-23 08:51:44 +0700

    trang dang bai

--- Project/controllers/uController.js
+++ Project/controllers/uController.js
@@ -2,15 +2,14 @@ var express = require('express'),
     q = require('q'),
     multer = require('multer'),
     fs = require('fs'),
+    path = require('path'),
     userRepo = require('../models/userRepo');
 
 var r = express.Router();
 
 r.get('/',function (req,res) {
 
-            res.render('dangBai',{
-                path:'/images/postImgBg.png'
-            });
+            res.render('dangBai',{});
 });
 
 
@@ -18,7 +17,7 @@ var storage = multer.diskStorage({
     //duong dan luu file
     destination: function (req,file,cb) {
         try{
-            fs.mkdirSync('./data/users/'+req.body.txt_tenSp);
+            fs.mkdirSync('./data/users/6');
             console.log('tao thanh cong')
         }catch(err) {
             if(err.code == 'EEXIST'){
@@ -27,18 +26,32 @@ var storage = multer.diskStorage({
                 console.log('Loi nhieu vl');
             }
         }
-        cb(null,'./data/users/'+req.body.txt_tenSp+'/')
+        cb(null,'./data/users/6/')
     },
     //ten file luu
     filename: function (req,file,cb) {
-        cb(null,file.originalname)
+        var ext = path.extname(file.originalname)
+        console.log(file.fieldname)
+        if(file.fieldname.toString().trim() === 'file1'){
+            cb(null,'1'+ext)
+        }else{
+            if(file.fieldname.toString().trim() === 'file2'){
+                cb(null,'2'+ext)
+            }else{
+                cb(null,'3'+ext)
+            }
+        }
     }
 })
 
-var upload = multer({storage:storage})
 
-r.post('/',upload.single('file1'),function (req,res) {
-    console.log(req.file.destination);
+
+r.post('/',function (req,res) {
+    var upload = multer({storage:storage}).any()
+    upload(req, res, function(err) {
+        console.log('Thanh coong ahihi');
+    })
+
 })
 
 module.exports = r;
