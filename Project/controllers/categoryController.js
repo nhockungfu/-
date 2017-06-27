f9c483941f4f6c4dd67d830b50db338554afe552
lvphuc253 <lvphucitus@gmail.com>
2017-06-27 15:51:30 +0700

    xong phần sắp xếp
    sắp xếp theo thời gian giảm
    sắp xếp giá cao nhất tăng

--- Project/controllers/categoryController.js
+++ Project/controllers/categoryController.js
@@ -14,7 +14,7 @@ r.get('/danhmuc/:id',function (req,res) {
         console.log(row[0].name);
         name=row[0].name;
     })
-    producesRepo.loadAllByCate(req.params.id, rec_per_page, offset)
+    producesRepo.loadAllByCate(req.params.id, rec_per_page, offset,0)
         .then(function(data) {
 
             var number_of_pages = data.total / rec_per_page;
@@ -36,8 +36,101 @@ r.get('/danhmuc/:id',function (req,res) {
                 layoutModels: res.locals.layoutModels,
                 produces: data.list,
                 isEmpty: data.total === 0,
+                cateId:req.params.id,
+                cateName: name,
+                sx:0,
+                type:0,
+                pages: pages,
+                curPage: curPage,
+                prevPage: curPage - 1,
+                nextPage: curPage + 1,
+                showPrevPage: curPage > 1,
+                showNextPage: curPage < number_of_pages - 1,
+            });
+        });
+
+})
+r.get('/danhmuc/sapxeptang/:id',function (req,res) {
+    var rec_per_page = 9;
+    var curPage = req.query.page ? req.query.page : 1;
+    var offset = (curPage - 1) * rec_per_page;
+    var name;
+    categoryRepo.getName(req.params.id).then(function (row) {
+        console.log(row[0].name);
+        name=row[0].name;
+    })
+    producesRepo.loadAllByCate(req.params.id, rec_per_page, offset,1)
+        .then(function(data) {
+
+            var number_of_pages = data.total / rec_per_page;
+            if (data.total % rec_per_page > 0) {
+                number_of_pages++;
+            }
+
+            var pages = [];
+            for (var i = 1; i <= number_of_pages; i++) {
+                pages.push({
+                    pageValue: i,
+                    isActive: function () {
+                        return i==curPage;
+                    }
+                });
+            }
+
+            res.render('category/dsSanPham', {
+                layoutModels: res.locals.layoutModels,
+                produces: data.list,
+                isEmpty: data.total === 0,
+                cateId:req.params.id,
                 cateName: name,
                 pages: pages,
+                sx:2,
+                type:0,
+                curPage: curPage,
+                prevPage: curPage - 1,
+                nextPage: curPage + 1,
+                showPrevPage: curPage > 1,
+                showNextPage: curPage < number_of_pages - 1,
+            });
+        });
+
+})
+r.get('/danhmuc/sapxepgiam/:id',function (req,res) {
+    var rec_per_page = 9;
+    var curPage = req.query.page ? req.query.page : 1;
+    var offset = (curPage - 1) * rec_per_page;
+    var name;
+    categoryRepo.getName(req.params.id).then(function (row) {
+        console.log(row[0].name);
+        name=row[0].name;
+    })
+    producesRepo.loadAllByCate(req.params.id, rec_per_page, offset,2)
+        .then(function(data) {
+
+            var number_of_pages = data.total / rec_per_page;
+            if (data.total % rec_per_page > 0) {
+                number_of_pages++;
+            }
+
+            var pages = [];
+            for (var i = 1; i <= number_of_pages; i++) {
+                pages.push({
+                    pageValue: i,
+                    isActive: function () {
+                        return i==curPage;
+                    }
+                });
+            }
+
+            res.render('category/dsSanPham', {
+                layoutModels: res.locals.layoutModels,
+                produces: data.list,
+                isEmpty: data.total === 0,
+                cateId:req.params.id,
+                cateName: name,
+                pages: pages,
+                sx:1,
+                type:0,
                 curPage: curPage,
                 prevPage: curPage - 1,
                 nextPage: curPage + 1,
@@ -55,7 +148,7 @@ r.post('/timkiem',function (req,res) {
     var name=req.body.txt_timKiem;
     req.session.cateName=name;
 
-    producesRepo.searchPro(name,rec_per_page, offset)
+    producesRepo.searchPro(name,rec_per_page, offset,0)
         .then(function(data) {
 
             var number_of_pages = data.total / rec_per_page;
@@ -77,8 +170,11 @@ r.post('/timkiem',function (req,res) {
                 layoutModels: res.locals.layoutModels,
                 produces: data.list,
                 isEmpty: data.total === 0,
-                cateName: 'Tìm kiếm: '+ name,
+                cate_name: 'Tìm kiếm: '+ name,
+                cateName:name,
                 pages: pages,
+                sx:0,
+                type:1,
                 curPage: curPage,
                 prevPage: curPage - 1,
                 nextPage: curPage + 1,
@@ -93,7 +189,7 @@ r.get('/timkiem',function (req,res) {
     var offset = (curPage - 1) * rec_per_page;
     var name=res.locals.layoutModels.cateName;
 
-    producesRepo.searchPro(name,rec_per_page, offset)
+    producesRepo.searchPro(name,rec_per_page, offset,0)
         .then(function(data) {
 
             var number_of_pages = data.total / rec_per_page;
@@ -115,8 +211,93 @@ r.get('/timkiem',function (req,res) {
                 layoutModels: res.locals.layoutModels,
                 produces: data.list,
                 isEmpty: data.total === 0,
-                cateName: 'Tìm kiếm: '+ name,
+                cate_name: 'Tìm kiếm: '+ name,
+                cateName:name,
                 pages: pages,
+                sx:0,
+                type:1,
+                curPage: curPage,
+                prevPage: curPage - 1,
+                nextPage: curPage + 1,
+                showPrevPage: curPage > 1,
+                showNextPage: curPage < number_of_pages - 1,
+            });
+        });
+})
+r.get('/timkiem/sapxeptang/:id',function (req,res) {
+    var rec_per_page = 9;
+    var curPage = req.query.page ? req.query.page : 1;
+    var offset = (curPage - 1) * rec_per_page;
+    var name=res.locals.layoutModels.cateName;
+
+    producesRepo.searchPro(name,rec_per_page, offset,1)
+        .then(function(data) {
+
+            var number_of_pages = data.total / rec_per_page;
+            if (data.total % rec_per_page > 0) {
+                number_of_pages++;
+            }
+
+            var pages = [];
+            for (var i = 1; i <= number_of_pages; i++) {
+                pages.push({
+                    pageValue: i,
+                    isActive: function () {
+                        return i==curPage;
+                    }
+                });
+            }
+
+            res.render('category/dsSanPham', {
+                layoutModels: res.locals.layoutModels,
+                produces: data.list,
+                isEmpty: data.total === 0,
+                cate_name: 'Tìm kiếm: '+ name,
+                cateName:name,
+                pages: pages,
+                sx:2,
+                type:1,
+                curPage: curPage,
+                prevPage: curPage - 1,
+                nextPage: curPage + 1,
+                showPrevPage: curPage > 1,
+                showNextPage: curPage < number_of_pages - 1,
+            });
+        });
+})
+r.get('/timkiem/sapxepgiam/:id',function (req,res) {
+    var rec_per_page = 9;
+    var curPage = req.query.page ? req.query.page : 1;
+    var offset = (curPage - 1) * rec_per_page;
+    var name=res.locals.layoutModels.cateName;
+
+    producesRepo.searchPro(name,rec_per_page, offset,2)
+        .then(function(data) {
+
+            var number_of_pages = data.total / rec_per_page;
+            if (data.total % rec_per_page > 0) {
+                number_of_pages++;
+            }
+
+            var pages = [];
+            for (var i = 1; i <= number_of_pages; i++) {
+                pages.push({
+                    pageValue: i,
+                    isActive: function () {
+                        return i==curPage;
+                    }
+                });
+            }
+
+            res.render('category/dsSanPham', {
+                layoutModels: res.locals.layoutModels,
+                produces: data.list,
+                isEmpty: data.total === 0,
+                cate_name: 'Tìm kiếm: '+ name,
+                cateName:name,
+                pages: pages,
+                sx:1,
+                type:1,
                 curPage: curPage,
                 prevPage: curPage - 1,
                 nextPage: curPage + 1,
